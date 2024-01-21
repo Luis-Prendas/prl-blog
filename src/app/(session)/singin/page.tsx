4a4c3e6d34b0a942480bcc963342c8aea18fe557
Luis Daniel Prendas Chavarria <luisprendas.dev@gmail.com>
2024-01-20 23:27:40 -0600

    option to login with a guest acount

--- src/app/(session)/singin/page.tsx
+++ src/app/(session)/singin/page.tsx
@@ -1,8 +1,9 @@
 import { Pulse } from '@/components/pulse'
+import Link from 'next/link'
 
 export default function SingInPage () {
   return (
-    <section className='w-full flex justify-center'>
+    <section className='w-full flex flex-col items-center justify-center gap-8'>
       <div className='w-full flex justify-center -z-10 absolute top-28'>
         <Pulse />
       </div>
@@ -35,6 +36,11 @@ export default function SingInPage () {
           </div>
         </section>
       </main>
+      <article>
+        <p>
+          To login as a guest click <Link href='/' className='inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-transparent'> here</Link>
+        </p>
+      </article>
     </section>
   )
 };
