593d6b6e9ff9d53dc6027a65c5eb2ececdbcf39e
Luis Daniel Prendas Chavarria <luisprendas.dev@gmail.com>
2024-01-21 00:02:14 -0600

    add shadcn to proyect

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,62 @@
 import type { Config } from 'tailwindcss'
 
-const config: Config = {
-  darkMode: 'class',
+const config = {
+  darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}'
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        }
       },
-      backgroundColor: {
-        1: '#8678F9',
-        2: '#E2CBFF',
-        3: '#393BB2'
-      },
-      animation: {
-        'background-shine': 'background-shine 2s linear infinite',
-        'border-width': 'border-width 3s infinite alternate',
-        'text-gradient': 'text-gradient 1.5s linear infinite'
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
       },
       keyframes: {
         'background-shine': {
@@ -47,10 +81,26 @@ const config: Config = {
           to: {
             backgroundPosition: '200% center'
           }
+        },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
         }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        'background-shine': 'background-shine 2s linear infinite',
+        'border-width': 'border-width 3s infinite alternate',
+        'text-gradient': 'text-gradient 1.5s linear infinite'
       }
     }
   },
-  plugins: []
-}
+  plugins: [require('tailwindcss-animate')]
+} satisfies Config
+
 export default config
